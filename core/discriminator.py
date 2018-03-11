f529ad73184b96df269cec7d9407deddd683e655
Pratyush More <pratyushmore96@gmail.com>
2018-03-11 20:33:30 +0800

    Fixed indentation

--- core/discriminator.py
+++ core/discriminator.py
@@ -2,45 +2,45 @@ import tensorflow as tf
 
 class Discriminator(object):
 	def __init__(self, word_to_idx, dim_feature=[196, 512], dim_embed=512, dim_hidden=1024, n_time_step=16,
-                  prev2out=True, ctx2out=True, alpha_c=0.0, selector=True, dropout=True, learning_rate=0.1):
-        """
-        Args:
-            word_to_idx: word-to-index mapping dictionary.
-            dim_feature: (optional) Dimension of vggnet19 conv5_3 feature vectors.
-            dim_embed: (optional) Dimension of word embedding.
-            dim_hidden: (optional) Dimension of all hidden state.
-            n_time_step: (optional) Time step size of LSTM.
-            prev2out: (optional) previously generated word to hidden state. (see Eq (7) for explanation)
-            ctx2out: (optional) context to hidden state (see Eq (7) for explanation)
-            alpha_c: (optional) Doubly stochastic regularization coefficient. (see Section (4.2.1) for explanation)
-            selector: (optional) gating scalar for context vector. (see Section (4.2.1) for explanation)
-            dropout: (optional) If true then dropout layer is added.
-        """
+	prev2out=True, ctx2out=True, alpha_c=0.0, selector=True, dropout=True, learning_rate=0.1):
+		"""
+		Args:
+		word_to_idx: word-to-index mapping dictionary.
+		dim_feature: (optional) Dimension of vggnet19 conv5_3 feature vectors.
+		dim_embed: (optional) Dimension of word embedding.
+		dim_hidden: (optional) Dimension of all hidden state.
+		n_time_step: (optional) Time step size of LSTM.
+		prev2out: (optional) previously generated word to hidden state. (see Eq (7) for explanation)
+		ctx2out: (optional) context to hidden state (see Eq (7) for explanation)
+		alpha_c: (optional) Doubly stochastic regularization coefficient. (see Section (4.2.1) for explanation)
+		selector: (optional) gating scalar for context vector. (see Section (4.2.1) for explanation)
+		dropout: (optional) If true then dropout layer is added.
+		"""
 
-        self.word_to_idx = word_to_idx
-        self.idx_to_word = {i: w for w, i in word_to_idx.iteritems()}
-        self.prev2out = prev2out
-        self.ctx2out = ctx2out
-        self.alpha_c = alpha_c
-        self.selector = selector
-        self.dropout = dropout
-        self.V = len(word_to_idx)
-        self.L = dim_feature[0]
-        self.D = dim_feature[1]
-        self.M = dim_embed
-        self.H = dim_hidden
-        self.T = n_time_step
+		self.word_to_idx = word_to_idx
+		self.idx_to_word = {i: w for w, i in word_to_idx.iteritems()}
+		self.prev2out = prev2out
+		self.ctx2out = ctx2out
+		self.alpha_c = alpha_c
+		self.selector = selector
+		self.dropout = dropout
+		self.V = len(word_to_idx)
+		self.L = dim_feature[0]
+		self.D = dim_feature[1]
+		self.M = dim_embed
+		self.H = dim_hidden
+		self.T = n_time_step
 		self.learning_rate = learning_rate
-        self._start = word_to_idx['<START>']
-        self._null = word_to_idx['<NULL>']
+		self._start = word_to_idx['<START>']
+		self._null = word_to_idx['<NULL>']
 
 		self.weight_initializer = tf.contrib.layers.xavier_initializer()
-        self.const_initializer = tf.constant_initializer(0.0)
-        self.emb_initializer = tf.random_uniform_initializer(minval=-1.0, maxval=1.0)
+		self.const_initializer = tf.constant_initializer(0.0)
+		self.emb_initializer = tf.random_uniform_initializer(minval=-1.0, maxval=1.0)
 
 		# Place holder for features and captions
-        self.features = tf.placeholder(tf.float32, [None, self.L, self.D])
-        self.captions = tf.placeholder(tf.int32, [None, self.T + 1])
+		self.features = tf.placeholder(tf.float32, [None, self.L, self.D])
+		self.captions = tf.placeholder(tf.int32, [None, self.T + 1])
 		self.target = tf.placeholder(tf.float32, [None, 1])
 		self.loss = None
 
@@ -49,7 +49,7 @@ class Discriminator(object):
 			return self.loss
 
 		features = self.features
-        captions = self.captions
+		captions = self.captions
 		target = self.target
 
 		features_flat = tf.reshape(features, [-1, 196 * 512])
@@ -81,10 +81,10 @@ class Discriminator(object):
 		sess.run(init_op)
 		fd_train = {features: image_features, captions: image_captions, target: y}
 		train_step.run(fd_train)
-        loss_step = loss.eval(fd_train)
-        print('  step: loss = %6d: %8.3f' % (i, loss_step)
+		loss_step = loss.eval(fd_train)
+		print('  step: loss = %6d: %8.3f' % (i, loss_step)
 		return loss_step
-		
+
 	def save(self):
 		pass
 
@@ -100,8 +100,8 @@ class Discriminator(object):
 			sess.run(init_op)
 			print 'Training Discriminator...'
 			fd_train = {features: image_features, captions: image_captions, target: y}
-		    pred = pred_sigmoid.eval(fd_train)
-	        return pred
+			pred = pred_sigmoid.eval(fd_train)
+			return pred
 
 	def get_grads(self, rewards):
 		pass
