00dd347444135613ff5e0644769eb5173e141b71
Pratyush More <pratyushmore96@gmail.com>
2018-03-14 18:53:11 +0800

    training discrim added

--- train_discrim.py
+++ train_discrim.py
@@ -16,9 +16,10 @@ def main():
 	discrim = Discriminator(word_to_idx, dim_feature=[196, 512], dim_embed=512, dim_hidden=1024, n_time_step=16,
                          prev2out=True, ctx2out=True, alpha_c=0.0, selector=True, dropout=True, learning_rate=0.01)
 
-	solver = CaptioningSolver(model, discrim, data, data, n_epochs=20, batch_size=128, update_rule='adam',
+	solver = CaptioningSolver(model, discrim, data, data, n_epochs=20, batch_size=128, gpu_list="1,2,3", update_rule='adam',
 								learning_rate=0.001, print_every=1000, save_every=1, image_path='./image/',
-								pretrained_model=None, model_path='model/lstm/', test_model='model/lstm/model-21',
+								pretrained_model=None, model_path='model/lstm/', train_new='./model/lstm/model-20',
+								test_model='model/lstm/model-21',
 								print_bleu=True, log_path='log/')
 
 	solver.train()
